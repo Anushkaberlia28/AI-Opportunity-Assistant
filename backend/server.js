e106c1abc2357191fec7fd4d598450ce107b749c
Anushka berlia <anushkaberlia@Anushkas-MacBook-Air.local>
2026-06-18 12:54:04 +0530

    User registration API completed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
@@ -10,7 +11,12 @@ connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Backend is running 🚀");
