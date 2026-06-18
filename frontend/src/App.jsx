98a7151d70ac46f23a2f440d48b0d544461d7849
Anushka berlia <anushkaberlia@Anushkas-MacBook-Air.local>
2026-06-18 11:11:41 +0530

    Completed backend setup and user model

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 // import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
 
 // function App() {
 //   return (
-//     <Login />
+//     <>
+//       <Login />
+//       <Dashboard />
+//     </>
 //   );
 // }
 
 // export default App;
+
+// import Login from "./pages/Login";
+
+// function App() {
+//   return <Login />;
+// }
+
+// export default App;
+
 import Dashboard from "./pages/Dashboard";
 
 function App() {
