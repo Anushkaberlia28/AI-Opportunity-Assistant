82563513e9bc622287079366845c177820135ec1
Anushka Berlia <anushkaberlia@gmail.com.com>
2026-06-19 12:10:53 +0530

    use state use ans local storage

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,12 +11,44 @@ const registerUser = async (req, res) => {
         });
 
         res.status(201).json(user);
-    }
-    catch (error) {
+    } catch (error) {
         res.status(500).json({
             message: error.message
         });
     }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        if (user.password !== password) {
+            return res.status(400).json({
+                message: "Invalid password"
+            });
+        }
+
+        res.status(200).json({
+            message: "Login Successful",
+            user
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+module.exports = {
+    registerUser,
+    loginUser
+};
